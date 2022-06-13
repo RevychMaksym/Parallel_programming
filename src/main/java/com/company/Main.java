36ecf84fd3b9444134a038629549365f9df0cb42
Max <login@gmail.com>
2022-06-13 22:31:45 +0300

    Test commit to a correct branch

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,4 +1,10 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
 public class Main {
 }
